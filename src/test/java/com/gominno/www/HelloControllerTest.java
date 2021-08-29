46661bfe3ceb6468fa4a192f58b2348bf81c7681
youngho <youngho7482@gmail.com>
2021-08-29 18:34:40 +0900

    FirstCommit

--- src/test/java/com/gominno/www/HelloControllerTest.java
+++ src/test/java/com/gominno/www/HelloControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-impo
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 public class HelloControllerTest {
 }
